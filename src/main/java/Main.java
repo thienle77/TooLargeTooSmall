6c3079c2b987ffa666dc6eea6bc6df3f83acbdfc
thienle77 <tle9677@gmail.com>
2022-11-02 11:09:24 -0400

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        int randomNumber;
+        randomNumber = (int)(Math.random() * 100);
+        System.out.println("Welcome to the Guessing Game!");
+        Scanner scanner = new Scanner (System.in);
+
+
+        int guess;
+        do {
+            System.out.println("Guess a number between 1 and 100");
+            guess = scanner.nextInt();
+            System.out.println("Your guess is " + guess);
+
+            if (guess == randomNumber)
+                System.out.println("Congrats your guess is correct!");
+            else if (guess < randomNumber)
+                System.out.println("Your number is too small");
+            else if (guess > randomNumber)
+                System.out.println("Your number is too large");
+        } while  (guess != randomNumber);
     }
 }
